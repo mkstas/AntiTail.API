25c524d67c80193773deca10ab16ce9ef57bfb17
mkstas <makarov.stn@gmail.com>
2025-09-26 18:08:44 +0300

    feat(api): register AntiTailDbContext

--- AntiTail.API/Program.cs
+++ AntiTail.API/Program.cs
@@ -1,7 +1,17 @@
+using AntiTail.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AntiTailDbContext>(
+    options =>
+    {
+        options.UseNpgsql(configuration.GetConnectionString(nameof(AntiTailDbContext)));
+    });
 
 var app = builder.Build();
 

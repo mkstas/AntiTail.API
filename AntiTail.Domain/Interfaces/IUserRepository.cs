3a247ef27335c7c6d50b9879f5106391c9622594
mkstas <makarov.stn@gmail.com>
2025-09-29 20:25:42 +0300

    feat(domain): create interfaces for user repository, service

---
+++ AntiTail.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using AntiTail.Domain.Models;
+
+namespace AntiTail.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<List<UserEntity>> GetAll();
+        Task<UserEntity?> GetById(long id);
+        Task<UserEntity?> GetByLogin(string login);
+        Task<UserEntity?> Create(string login);
+        Task<UserEntity?> Update(long id, string login);
+        Task<bool> Delete(long id);
+    }
+}
---
+++ AntiTail.Domain/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using AntiTail.Domain.Models;
+
+namespace AntiTail.Domain.Interfaces
+{
+    public interface IUserService
+    {
+        Task<List<UserEntity>> GetUsers();
+        Task<UserEntity?> GetUserById(long id);
+        Task<UserEntity?> GetUserByLogin(string login);
+        Task<UserEntity?> CreateUser(string login);
+        Task<UserEntity?> UpdateUser(long id, string login);
+        Task<bool> DeleteUser(long id);
+    }
+}

6c46a2dbcd773c964cf8cb7653b79739404ab162
mkstas <makarov.stn@gmail.com>
2025-10-01 22:08:09 +0300

    feat(persistence): create subject repository

---
+++ AntiTail.Persistence/Repositories/SubjectRepository.cs
@@ -0,0 +1,59 @@
+﻿using AntiTail.Domain.Interfaces.Subject;
+using AntiTail.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AntiTail.Persistence.Repositories
+{
+    public class SubjectRepository(AntiTailDbContext dbContext) : ISubjectRepository
+    {
+        private readonly AntiTailDbContext _dbContext = dbContext;
+
+        public async Task<List<SubjectEntity>> GetAll()
+        {
+            return await _dbContext.Subjects
+                .ToListAsync();
+        }
+
+        public async Task<SubjectEntity?> GetById(long id)
+        {
+            return await _dbContext.Subjects
+                .FirstOrDefaultAsync(s => s.Id == id);
+        }
+
+        public async Task<SubjectEntity?> Create(long userId, string title)
+        {
+            var subject = new SubjectEntity(userId, title);
+
+            await _dbContext.AddAsync(subject);
+            await _dbContext.SaveChangesAsync();
+
+            return subject;
+        }
+
+        public async Task<SubjectEntity?> Update(long id, string title)
+        {
+            var updatedRows = await _dbContext.Subjects
+                .Where(s => s.Id == id)
+                .ExecuteUpdateAsync(
+                    s => s
+                    .SetProperty(s => s.Title, title));
+
+            if (updatedRows == 0)
+            {
+                return null;
+            }
+
+            return await _dbContext.Subjects
+                .FirstOrDefaultAsync(s => s.Id == id);
+        }
+
+        public async Task<bool> Delete(long id)
+        {
+            var deletedRows = await _dbContext.Subjects
+                .Where(s => s.Id == id)
+                .ExecuteDeleteAsync();
+
+            return deletedRows > 0;
+        }
+    }
+}

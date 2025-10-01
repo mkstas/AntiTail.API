3bc279881840d2199e06cba3a77ceb529ae1735a
mkstas <makarov.stn@gmail.com>
2025-10-01 22:07:26 +0300

    feat(domain): create subject interfaces for repository, service

---
+++ AntiTail.Domain/Interfaces/Subject/ISubjectRepository.cs
@@ -0,0 +1,17 @@
+﻿using AntiTail.Domain.Models;
+
+namespace AntiTail.Domain.Interfaces.Subject
+{
+    public interface ISubjectRepository
+    {
+        Task<List<SubjectEntity>> GetAll();
+
+        Task<SubjectEntity?> GetById(long id);
+
+        Task<SubjectEntity?> Create(long userId, string title);
+
+        Task<SubjectEntity?> Update(long id, string title);
+
+        Task<bool> Delete(long id);
+    }
+}
---
+++ AntiTail.Domain/Interfaces/Subject/ISubjectService.cs
@@ -0,0 +1,17 @@
+﻿using AntiTail.Domain.Models;
+
+namespace AntiTail.Domain.Interfaces.Subject
+{
+    public interface ISubjectService
+    {
+        Task<List<SubjectEntity>> GetAllSubjects();
+
+        Task<SubjectEntity?> GetSubjectById(long id);
+
+        Task<SubjectEntity?> CreateSubject(long userId, string title);
+
+        Task<SubjectEntity?> UpdateSubject(long id, string title);
+
+        Task<bool> Delete(long id);
+    }
+}
